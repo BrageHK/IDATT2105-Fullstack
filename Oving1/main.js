8f8d9287d37916a025d4350e077315ae388aee31
BrageNTNU <bragehk@stud.ntnu.no>
2023-01-13 11:42:23 +0100

    Website is now fully working, but is kinda ugly

--- Oving1/main.js
+++ Oving1/main.js
@@ -1,18 +1,47 @@
+var counter = 0;
+Boolean = true; 
 
-
-var button = document.getElementById("myButton");
-var counter = document.getElementById("counter");
-
-var count = 0;
-
-document.getElementById("myButton").onclick = function() {
-    count++;
-    counter.innerHTML = count;
-    alert("Button was clicked!");
+function increment() {
+    counter++;
+    document.getElementById("counter").innerHTML = "The value of the counter is " + counter;
 }
 
-button.addEventListener("click", function() {
-    count++;
-    counter.innerHTML = count;
-    alert("Button was clicked!");
-});
+function hide() {
+    if(Boolean == true) {
+        document.getElementById("counter").style.visibility = "hidden";
+        Boolean = false;
+    } else {
+        document.getElementById("counter").style.visibility = "visible";
+        Boolean = true;
+    }
+}
+
+function getRandomWords() {
+    document.getElementById("word1").innerHTML = getRandomWord();
+    document.getElementById("word2").innerHTML = getRandomWord();
+    document.getElementById("word3").innerHTML = getRandomWord();
+}
+
+// Jank metode fordi det er mange tomme strings etter script.split(" ")
+function getRandomWord() {
+    var script = document.getElementById("beeScript").innerHTML;
+    var words = script.split(" ");
+    var word = "";
+    while(word.length==0) {
+        word = words[Math.floor(Math.random() * words.length)];
+    }
+    if(word == "bee") {
+        word = "BEE";
+    }
+    return word + " (Word appears " + getWordCount(words, word) + " times)";
+}
+
+function getWordCount(words, word) {
+    var wordCount = 0;
+    for(var i = 0; i < words.length; i++) {
+        if(words[i] == word) {
+            wordCount++;
+        }
+    }
+    return wordCount;
+}
